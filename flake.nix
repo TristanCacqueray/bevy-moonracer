265417f5daa5d84323aae13b314c97add0318cd6
Tristan Cacqueray <tdecacqu@redhat.com>
2023-09-26 23:10:29 +0000

    Initial summon

---
+++ flake.nix
@@ -0,0 +1,96 @@
+# Build release with: nix -L build .#release
+{
+  description = "bevy-moonracer";
+
+  inputs = {
+    # nixpkgs is tracking nixpkgs-unstable
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/3d6ebeb283be256f008541ce2b089eb5fb0e4e01";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane/8b4f7a4dab2120cf41e7957a28a853f45016bd9d";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url =
+        "github:oxalica/rust-overlay/46dbbcaf435b0d22b149684589b9b059f73f4ffc";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, crane, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" "wasm32-unknown-unknown" ];
+        };
+        linker = with pkgs; {
+          "x86_64-linux" = mold;
+          "x86_64-darwin" = zld;
+          "aarch64-darwin" = zld;
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.; # The original, unfiltered source
+          filter = path: type:
+            (pkgs.lib.hasSuffix ".html" path)
+            || (pkgs.lib.hasSuffix ".config.js" path)
+            || (pkgs.lib.hasSuffix ".css" path)
+            || (pkgs.lib.hasSuffix ".md" path)
+            || (pkgs.lib.hasSuffix ".txt" path) ||
+            # Default filter from crane (allow .rs files)
+            (craneLib.filterCargoSources path type);
+        };
+        web-info = {
+          src = src;
+        } // craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+        cargoArtifactsWasm = craneLib.buildDepsOnly (web-info // {
+          doCheck = false;
+          CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+        });
+        web = craneLib.buildTrunkPackage (web-info // {
+          src = src;
+          cargoToml = ./Cargo.toml;
+          cargoArtifacts = cargoArtifactsWasm;
+          trunkIndexPath = "./index.html";
+        });
+
+      in {
+        packages.web = web;
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+            clang
+
+            udev
+            alsa-lib
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr # To use the x11 feature
+            libxkbcommon
+            wayland # To use the wayland feature
+
+            trunk
+          ];
+
+          nativeBuildInputs = with pkgs;
+            [ pkg-config ] ++ pkgs.lib.catAttrs system [ linker ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,162 @@
+//! Demo example copied from bloom_3d.rs
+
+use bevy::{
+    core_pipeline::{bloom::BloomSettings, tonemapping::Tonemapping},
+    prelude::*,
+};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::DARK_GRAY))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_scene)
+        .add_systems(Update, (update_bloom_settings, bounce_spheres))
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    ));
+
+    let material_emissive1 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(13.99, 5.32, 2.0), // 4. Put something bright in a dark environment to see the effect
+        ..default()
+    });
+    let material_emissive2 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(2.0, 13.99, 5.32),
+        ..default()
+    });
+    let material_emissive3 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(5.32, 2.0, 13.99),
+        ..default()
+    });
+    let material_non_emissive = materials.add(StandardMaterial {
+        base_color: Color::GRAY,
+        ..default()
+    });
+
+    let mesh = meshes.add(
+        shape::Icosphere {
+            radius: 0.5,
+            subdivisions: 5,
+        }
+        .try_into()
+        .unwrap(),
+    );
+
+    for x in -5..5 {
+        for z in -5..5 {
+            let mut hasher = DefaultHasher::new();
+            (x, z).hash(&mut hasher);
+            let rand = (hasher.finish() - 2) % 6;
+
+            let material = match rand {
+                0 => material_emissive1.clone(),
+                1 => material_emissive2.clone(),
+                2 => material_emissive3.clone(),
+                3..=5 => material_non_emissive.clone(),
+                _ => unreachable!(),
+            };
+
+            commands.spawn((
+                PbrBundle {
+                    mesh: mesh.clone(),
+                    material,
+                    transform: Transform::from_xyz(x as f32 * 2.0, 0.0, z as f32 * 2.0),
+                    ..default()
+                },
+                Bouncing,
+            ));
+        }
+    }
+
+    // example instructions
+    commands.spawn(
+        TextBundle::from_section(
+            "",
+            TextStyle {
+                font_size: 20.0,
+                color: Color::BLACK,
+                ..default()
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        }),
+    );
+}
+
+// ------------------------------------------------------------------------------------------------
+
+fn update_bloom_settings(
+    mut camera: Query<(Entity, Option<&mut BloomSettings>), With<Camera>>,
+    mut text: Query<&mut Text>,
+    mut commands: Commands,
+    keycode: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let bloom_settings = camera.single_mut();
+    let mut text = text.single_mut();
+    let text = &mut text.sections[0].value;
+
+    match bloom_settings {
+        (entity, Some(mut bloom_settings)) => {
+            *text = "BloomSettings (Toggle: Space)\n".to_string();
+            text.push_str(&format!("(Q/A) Intensity: {}\n", bloom_settings.intensity));
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).remove::<BloomSettings>();
+            }
+
+            let dt = time.delta_seconds();
+
+            if keycode.pressed(KeyCode::A) {
+                bloom_settings.intensity -= dt / 8.0;
+            }
+            if keycode.pressed(KeyCode::Q) {
+                bloom_settings.intensity += dt / 8.0;
+            }
+            bloom_settings.intensity = bloom_settings.intensity.clamp(0.0, 1.0);
+        }
+
+        (entity, None) => {
+            *text = "Bloom: Off (Toggle: Space)".to_string();
+
+            if keycode.just_pressed(KeyCode::Space) {
+                commands.entity(entity).insert(BloomSettings::default());
+            }
+        }
+    }
+}
+
+#[derive(Component)]
+struct Bouncing;
+
+fn bounce_spheres(time: Res<Time>, mut query: Query<&mut Transform, With<Bouncing>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.y =
+            (transform.translation.x + transform.translation.z + time.elapsed_seconds()).sin();
+    }
+}
